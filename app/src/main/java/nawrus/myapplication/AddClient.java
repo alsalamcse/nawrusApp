3acfb949faabacabdcc60a29a794a8f8936759ab
Owner <alsalamcse>
2020-02-23 09:28:02 +0200

    add client add manager

--- app/src/main/java/nawrus/myapplication/AddClient.java
+++ app/src/main/java/nawrus/myapplication/AddClient.java
@@ -1,16 +1,28 @@
 package nawrus.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import nawrus.myapplication.data.MyClient;
 
 public class AddClient extends AppCompatActivity {
     private EditText edtNamee;
     private EditText edtPhone5;
     private EditText edtEmail2;
     private EditText edtPass;
-
+    private Button btnSaveClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,69 @@ public class AddClient extends AppCompatActivity {
         edtPhone5=(EditText)findViewById(R.id.edtPhone5);
         edtEmail2=(EditText)findViewById(R.id.edtEmail2);
         edtPass=(EditText)findViewById(R.id.edtPass);
+        btnSaveClient=(Button)findViewById(R.id.btnSaveClient);
+
+        btnSaveClient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataHandler();
+            }
+        });
 
     }
+
+
+    private void dataHandler() {
+        String name1 = edtNamee.getText().toString();
+        String phone1 = edtPhone5.getText().toString();
+        String email1 = edtEmail2.getText().toString();
+        String password1 = edtPass.getText().toString();
+        boolean isOk = true;
+        if (name1.length() < 1) {
+            edtNamee.setError("title length error");
+            isOk = false;
+
+        }
+        if (phone1.length() < 10) {
+            edtPhone5.setError(" subject length error ");
+            isOk = false;
+        }
+        if (isOk) {
+            MyClient d = new MyClient();
+            d.setName1(name1);
+            createMyTask(d);
+
+        }
+    }
+
+    private void createMyTask(MyClient d) {
+        //.1
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //.2
+        DatabaseReference reference = database.getReference();
+        FirebaseAuth auth = FirebaseAuth.getInstance();//to get the user uid(or other details like email)
+        String uid = auth.getCurrentUser().getUid();
+        String key = reference.child("task").push().getKey();
+        d.setKey(key);
+        reference.child(key).setValue(d).addOnCompleteListener(AddClient.this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(AddClient.this, "add successful", Toast.LENGTH_SHORT).show();
+                    finish();
+                } else {
+                    Toast.makeText(AddClient.this, "add failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    task.getException().printStackTrace();
+
+                }
+
+            }
+
+
+        });
+    }
 }
+
+
+
+
--- app/src/main/java/nawrus/myapplication/data/MyClient.java
+++ app/src/main/java/nawrus/myapplication/data/MyClient.java
@@ -2,4 +2,61 @@ package nawrus.myapplication.data;
 
 public class MyClient {
 
+    private String key;
+    private String name1;
+    private String phone1;
+    private String email1;
+    private String password1;
+
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName1() {
+        return name1;
+    }
+
+    public void setName1(String name1) {
+        this.name1 = name1;
+    }
+
+    public String getPhone1() {
+        return phone1;
+    }
+
+    public void setPhone1(String phone1) {
+        this.phone1 = phone1;
+    }
+
+    public String getEmail1() {
+        return email1;
+    }
+
+    public void setEmail1(String email1) {
+        this.email1 = email1;
+    }
+
+    public String getPassword1() {
+        return password1;
+    }
+
+    public void setPassword1(String password1) {
+        this.password1 = password1;
+    }
+
+    @Override
+    public String toString() {
+        return "MyClient{" +
+                "key='" + key + '\'' +
+                ", name1='" + name1 + '\'' +
+                ", phone1='" + phone1 + '\'' +
+                ", email1='" + email1 + '\'' +
+                ", password1='" + password1 + '\'' +
+                '}';
+    }
 }
