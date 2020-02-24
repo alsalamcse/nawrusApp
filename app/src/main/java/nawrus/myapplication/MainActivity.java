9ee58d7856225d2f8dd979056eb2e8613807092d
Owner <alsalamcse>
2020-02-24 10:48:48 +0200

    add client add manager

--- app/src/main/java/nawrus/myapplication/MainActivity.java
+++ app/src/main/java/nawrus/myapplication/MainActivity.java
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnManager;
+    private Button btnClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnManager=(Button)findViewById(R.id.btnManager);
+        btnClient=(Button)findViewById(R.id.btnClient);
+
+
     }
     @Override
     protected void onResume() {
@@ -22,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
             public void run()
             {
                 try {
-                    sleep(3*1000);
+                    sleep(10*1000);
                     FirebaseAuth auth=FirebaseAuth.getInstance();
                     if(auth.getCurrentUser()==null || auth.getCurrentUser().getEmail()==null){
                         Intent i=new Intent(getApplication(),SignIn.class);
